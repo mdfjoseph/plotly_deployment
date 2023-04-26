890a0b82bc76f92a9a2a5d0a0f3aecd333faf1c4
Marcia Joseph <mdfugh@yahoo.com>
2023-04-26 16:31:35 -0500

    Module 13 Challenge

--- charts.js
+++ charts.js
@@ -61,48 +61,119 @@ function buildCharts(sample) {
     console.log(data);
 
     // Deliverable 1: 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
+   
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
+    var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
 
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata = data.metadata;
+    var metaArray = metadata.filter(sampleObj => sampleObj.id == sample);
 
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
+    var result = sampleArray[0];
 
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
+    var result1 = metaArray[0];
 
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+    console.log(otu_ids)
+    console.log(otu_labels)
+    console.log(sample_values)
 
     // Deliverable 3: 3. Create a variable that holds the washing frequency.
+    var washing = parseFloat(result1.wfreq)
+   console.log(washing)
+
+
 
 
     // Deliverable 1: 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
     // so the otu_ids with the most bacteria are last. 
-    var yticks = 
+    var yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
 
     // Deliverable 1: 8. Create the trace for the bar chart. 
-    var barData = [
+    var barData = [{
+      x: sample_values.slice(0,10).reverse(),
+      y: yticks,
+      text: otu_labels.slice(0,10).reverse(),
+      type:'bar',
+      orientation: 'h'
+      
+    }
 
     ];
 
     // Deliverable 1: 9. Create the layout for the bar chart. 
     var barLayout = {
+      title: "Top 10 Bacterial Species",
+    xaxis: {title: "Sample Values"},
+    yaxis: {title: "ID's"}
 
     };
 
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar",barData, barLayout);
 
     // Deliverable 2: 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_ids.slice(0,10).reverse(),
+      y: sample_values.slice(0,10).reverse(),
+      text: otu_labels.slice(0,10).reverse(),
+      mode: 'markers',
+      marker: {
+        size: sample_values.slice(0,10).reverse(),
+        color: otu_ids.slice(0,10).reverse(),
+        colorscale: 'Earth'
+      }
+      
+    }
+   
+    ];
+
 
     // Deliverable 2: 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Top 10 Bacterial Species",
+      xaxis: {title: "Sample Values"},
+      yaxis: {title: "ID's"}
+      
+    };
 
     // Deliverable 2: 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble",bubbleData, bubbleLayout)
     
     // Deliverable 3: 4. Create the trace for the gauge chart.
+    var gaugeData = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: washing,
+        gauge: {
+          axis: {range : [null,10],tickwidth:1},
+          steps: [
+            { range: [0, 2], color: "red" },
+            { range: [2, 4], color: "orange" },
+            { range: [4, 6], color: "yellow" },
+            { range: [6, 8], color: "limegreen" },
+            { range: [8, 10], color: "blue" }]
+        },
+        title: { text: "Belly Button Washing Frequency" },
+        type: "indicator",
+        mode: "gauge+number"
+  
+      }
+    ];
+
     
     // Deliverable 3: 5. Create the layout for the gauge chart.
+    var gaugeLayout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
 
     // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge",gaugeData,gaugeLayout)
 
   });
 }
